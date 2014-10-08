d91639e8be0f7f1726d981d10ffc08c6c7f28955
LeeYoungIn <pimery@nate.com>
2014-10-08 16:19:36 +0900

    //

--- planner/src/com/planner/value/StringSet.java
+++ planner/src/com/planner/value/StringSet.java
@@ -1,5 +1,8 @@
 package com.planner.value;
 
+import com.planner.app.R;
+import com.planner.app.HomeActivity;
+
 public class StringSet {
 
 	public static final String DAY = "day";
@@ -8,5 +11,9 @@ public class StringSet {
 	public static final String TODAY = "today";
 	public static final String STARTDAY = "startday";
 	public static final String MAIN = "Main";
-	
+
+	public static final String DAILY = HomeActivity.me.getString(R.string.day_text);
+	public static final String WEEKLY = HomeActivity.me.getString(R.string.week_text);
+	public static final String MONTYLY = HomeActivity.me.getString(R.string.month_text);
+	public static final String CATEGORY = HomeActivity.me.getString(R.string.category_text);
 }
